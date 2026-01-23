2036718a2116b653f417bc1d71022a6f1d787b82
Tyson M <tysonjmiles@icloud.com>
2026-01-23 22:29:28 +1000

    Initial Commit

---
+++ OculaTests/OculaTests.swift
@@ -0,0 +1,17 @@
+//
+//  OculaTests.swift
+//  OculaTests
+//
+//  Created by Tyson Miles on 23/1/2026.
+//
+
+import Testing
+@testable import Ocula
+
+struct OculaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
